7b2c40f180457a3acd019d0b7f877080519f96b4
lucas2020109662 <lucas.2020109662@unicap.br>
2022-10-09 21:22:11 +0000

    Opções do main

--- FrontDesk/Menu.java
+++ FrontDesk/Menu.java
@@ -17,7 +17,25 @@ Patient pat1= new Patient("12345678900", "1207123", "caio", "aflitos", "19/02/03
         switch (op) {
             case 1:
                     System.out.println(pat1.personInfo());
-            break;
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 0:
+                System.out.println("Você fechou o programa!");
+                break;
+            default:
+                System.out.println("Número inválido, insira uma opção na tela");
+                break;
         
     
         }
@@ -26,7 +44,15 @@ Patient pat1= new Patient("12345678900", "1207123", "caio", "aflitos", "19/02/03
     }      
       
         public static void showMenu(){
-        System.out.println("1- in dev");
+        System.out.println("1 - Cadastrar um novo cliente \n" +
+                "2 - Cadastrar um novo funcionario \n" +
+                "3 - Marcar um atendimento \n" +
+                "4 - Gerenciar um atendimento \n" +
+                "5 - Alterar os dados de um cliente \n" +
+                "6 - Alterar os dados de um funcionario \n" +
+                "7 - Remover um cliente \n" +
+                "8 - Remover um funcionario \n" +
+                "0 - Sair do programa");
         System.out.print("choice: ");
         }    
 
